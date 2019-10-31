66802fa8e38873c4d2f48f01837d685010cbf654
Nathan Graule <solarliner@gmail.com>
2019-10-31 15:09:48 +0100

    Add documentation to disassembly function

--- src/disassemble.h
+++ src/disassemble.h
@@ -3,6 +3,13 @@
 
 #include "eva.h"
 
+/**
+ * @brief Disassemble a program into a textual assembly representation into
+ * stdout.
+ * ATTENTION: The resulting assembly code will often NOT be the same as the
+ * input assembly code.
+ * @param op Opcode to disassemble.
+ */
 void disassemble(opcode_t op);
 
 #endif
